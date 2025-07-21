9f2b0da2972dc7479dd746e6127a16627541a677
santiago <santiuribeduarte@gmail.com>
2025-07-21 11:58:17 -0500

    Feat: Doing half the work

---
+++ DIa_1/java/script.js
@@ -0,0 +1,40 @@
+//### ProyectoJS ###//
+
+
+boleano = true
+while (boleano = true) {
+    let opci = prompt(`
+        ============================================
+        Simulador de Gasto Diario
+        =============================================
+        Seleccione una opción:
+
+        1. Registrar nuevo gasto
+        2. Listar gastos
+        3. Calcular total de gastos
+        4. Generar reporte de gastos
+        5. Salir
+        =============================================`)
+
+    if (opci == 1) {
+        let montoGasto = parseInt(prompt(`
+        =============================================
+                Registrar Nuevo Gasto
+        =============================================
+            Ingrese la información del gasto:
+
+            Monto del gasto :
+        =============================================`))
+        let categoria = prompt(`  
+        =============================================
+                Registrar Nuevo Gasto
+        =============================================
+            Ingrese la información del gasto:
+            Categoria del gasto(comida,tecnologia,etc):
+        =============================================`)
+
+    }
+}
+
+
+// Desarrolado por Santiago Uribe Duarte //
