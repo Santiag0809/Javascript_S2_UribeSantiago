a1ac08eb3a2b678ba8a4843408f05285fd1dc684
Santiago <santiuribeduarte@gmail.com>
2025-07-23 11:22:56 -0500

    Feat: Adding the code for creating ingredients

---
+++ Dia_4/java/java.js
@@ -0,0 +1,87 @@
+ingredientes = []
+// CAFETERIA CAMPUS //
+
+let boleano = true
+while (boleano = true) {
+
+    let opci = prompt(`
+        ===========================================
+
+                    Simulador de Cafeteria
+        ===========================================
+        Seleccione una opción:
+
+        1. Crear nuevas opciones
+        2. Listar opciones
+        3. Actualizar opciones
+        4. Borrar opciones
+        5. Salir
+        ===========================================`)
+
+
+    if (opci == 1) {
+
+        let opciones = parseInt(prompt(`
+            ===========================================
+                        Registrar nueva opcion
+            ===========================================
+                Ingrese la opcion que desea añadir:
+    
+                1. ingrediente :
+                2. hamburguesa :
+                3. categorias :
+                4. chef :
+            ===========================================`));
+
+        if (opciones == 1) {
+            let nombreIng = prompt(`
+            ===========================================
+                    Registrar nombre ingrediente
+            ===========================================`);
+
+            let descriIng = prompt(`
+            ===========================================
+                    Registrar descripcion ingrediente
+            ===========================================`);
+
+            let precioIng = parseInt(prompt(`
+            ===========================================
+                    Registrar precio ingrediente
+            ===========================================`));
+
+            let stockIng = parseInt(prompt(`
+            ===========================================
+                    Registrar stock ingrediente
+            ===========================================`));
+
+            let guardar = prompt(`
+            ==========================================
+                    Registrar nuevo ingrediente
+            ==========================================
+
+                ¿Desea guardar el nuevo ingrediente?
+                1. Sí
+                2. No`);
+
+            if (guardar === "1") {
+                ingredientes.push({
+                    "Nombre": nombreIng,
+                    "Descripcion": descriIng,
+                    "Precio": precioIng,
+                    "Stock" : stockIng
+                });
+                alert("Ingrediente guardado correctamente.");
+            } else {
+                alert("Ingrediente no guardadito.");
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+// CREADO POR : SANTIAGO URIBE T.I 1.097.099.401 //
