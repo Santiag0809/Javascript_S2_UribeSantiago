839e400e3d098c89ea8d378ae327b434e44acd9d
Santiago <santiuribeduarte@gmail.com>
2025-07-30 17:56:44 -0500

    Fix: Some minor code has been fixed

--- DIa_1/java/script.js
+++ DIa_1/java/script.js
@@ -1,5 +1,4 @@
 
-
 gastos = []
 
 //### ProyectoJS ###//
@@ -76,62 +75,49 @@ while (boleano = true) {
             alert("Gasto no guardadito.");
         }
     }
+
     else if (opci == "2") {
         if (gastos.length === 0) {
-            alert("No hay gastos registrados.");
+            alert(" No hay gastos registrados.");
         } else {
-            let opcidos = prompt(`
-    ============================================
-        ¿Cómo desea listar los gastos?
-        1. Listar todos
-        2. Listar por categoría
-        3. Listar por fecha exacta (YYYY-MM-DD)
-    ============================================`);
-
-            if (opcidos === "1") {
+            let opcion2 = prompt(`
+        ============================================
+            ¿Cómo desea listar los gastos?
+                1. Listar todos
+                2. Listar por categoría
+                3. Listar por fecha exacta (YYYY-MM-DD)
+        ============================================`);
+            if (opcion2 === "1") {
                 let lista = "Lista de todos los gastos:\n\n";
                 gastos.forEach((gasto, index) => {
-                    lista += `${index + 1}. $${gasto.monto} - ${gasto.categoria} (${gasto.descripcion}) - fecha: ${gasto.fecha}\n`;
+                    lista += `${index + 1}. ${gasto.monto} - ${gasto.categoria} (${gasto.descripcion}) - fecha: ${gasto.fecha}\n`;
                 });
+
                 alert(lista);
             }
 
-        
-
-            else if (opcidos === "2") {
-                let categoriaFiltrar = prompt("Ingrese la categoria :");
-                let encontrados = gastos.filter(g => g.categoria === categoriaFiltrar);
-
-                if (encontrados.length == 0) {
-                    alert("No hay gastos registrados en esa categoria.");
-                } else {
-                    let lista = `Gastos de la categoria ${categoriaFiltrar}:\n\n`;
-                    encontrados.forEach((gasto, index) => {
-                        lista += `${index + 1}. $${gasto.monto} - ${gasto.categoria} (${gasto.descripcion}) - fecha: ${gasto.fecha}\n`;
-                    });
-                    alert(lista);
-                }
-
+            
+            else if (opcion2 === "2") {
+                prompt("funcion no implementada");
             }
 
-            else if (opcidos === "3") {
+            else if (opcion2 === "3") {
                 let fechaFiltrar = prompt("Ingrese la fecha (YYYY-MM-DD):");
-                let encontrados = gastos.filter(g => g.fecha === fechaFiltrar);
+                let encontrados = gastos.filter(gastito => gastito.fecha === fechaFiltrar);
 
                 if (encontrados.length === 0) {
-                    alert("No hay gastos registrados en esa fecha.");
+                    alert("No hubo registro de ningun gasto en esta fecha.");
                 } else {
                     let lista = `Gastos del día ${fechaFiltrar}:\n\n`;
                     encontrados.forEach((gasto, index) => {
-                        lista += `${index + 1}. $${gasto.monto} - ${gasto.categoria} (${gasto.descripcion}) - fecha: ${gasto.fecha}\n`;
+                        lista += `${index + 1}. ${gasto.monto} - ${gasto.categoria} (${gasto.descripcion})\n`;
                     });
                     alert(lista);
+                } 
                 }
 
-            }
-
             else {
-                alert("Opción no válida.");
+                alert("Opcion invalida.");
             }
         }
     }
